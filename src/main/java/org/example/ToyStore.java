7388676fc79776a5fac69674ff16841d1be4ebc9
Sergei <ip744692@yandex.ru>
2023-09-15 15:05:57 +0300

    add work_file

--- src/main/java/org/example/ToyStore.java
+++ src/main/java/org/example/ToyStore.java
@@ -1,20 +1,23 @@
+package org.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
-// Основной класс для розыгрыша игрушек
 public class ToyStore {
     private List<Toy> toys;
+    private List<Toy> prizeToys;
 
     public ToyStore() {
         toys = new ArrayList<>();
+        prizeToys = new ArrayList<>();
     }
 
-    // Метод для добавления новой игрушки
     public void addToy(int id, String name, int quantity, int weight) {
         Toy toy = new Toy(id, name, quantity, weight);
         toys.add(toy);
     }
 
-    // Метод для изменения веса (частоты выпадения) игрушки
     public void updateWeight(int id, int newWeight) {
         for (Toy toy : toys) {
             if (toy.getId() == id) {
@@ -25,9 +28,34 @@ public class ToyStore {
         System.out.println("Игрушка с ID " + id + " не найдена.");
     }
 
-    // Метод для проведения розыгрыша
+    public void addPrizeToy(Toy toy) {
+        if (toys.contains(toy) && toy.getQuantity() > 0) {
+            prizeToys.add(toy);
+        } else {
+            System.out.println("Ошибка: Игрушка не может быть добавлена в список призовых.");
+        }
+    }
+
+    public Toy getPrizeToy() {
+        if (!prizeToys.isEmpty()) {
+            Toy prize = prizeToys.remove(0);
+            prize.decrementQuantity();
+            return prize;
+        } else {
+            System.out.println("Список призовых игрушек пуст.");
+            return null;
+        }
+    }
+
+    public void writePrizeToTextFile(Toy prize, String fileName) {
+        try (FileWriter fileWriter = new FileWriter(fileName, true)) {
+            fileWriter.write("Победила игрушка: " + prize.getName() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public Toy drawToy() {
-        // Создаем список с учетом весов (частот выпадения)
         List<Toy> weightedToys = new ArrayList<>();
         for (Toy toy : toys) {
             for (int i = 0; i < toy.getWeight(); i++) {
@@ -35,7 +63,6 @@ public class ToyStore {
             }
         }
 
-        // Выбираем случайную игрушку из списка с учетом весов
         Random random = new Random();
         int randomIndex = random.nextInt(weightedToys.size());
         return weightedToys.get(randomIndex);
@@ -44,23 +71,29 @@ public class ToyStore {
     public static void main(String[] args) {
         ToyStore toyStore = new ToyStore();
 
-        // Добавление новых игрушек
         toyStore.addToy(1, "Конструктор", 10, 20);
         toyStore.addToy(2, "Робот", 5, 10);
         toyStore.addToy(3, "Кукла", 15, 70);
 
-        // Изменение веса (частоты) игрушки
         toyStore.updateWeight(1, 30);
 
-        // Розыгрыш игрушек
-        for (int i = 0; i < 10; i++) {
-            Toy winner = toyStore.drawToy();
-            System.out.println("Победила игрушка: " + winner.getName());
+        for (int i = 0; i < 5; i++) {
+            Toy prizeToy = toyStore.drawToy();
+            if (prizeToy != null) {
+                toyStore.addPrizeToy(prizeToy);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            Toy prize = toyStore.getPrizeToy();
+            if (prize != null) {
+                toyStore.writePrizeToTextFile(prize, "prize_winners.txt");
+                System.out.println("Выдана призовая игрушка: " + prize.getName());
+            }
         }
     }
 }
 
-// Класс для игрушек
 class Toy {
     private int id;
     private String name;
@@ -92,5 +125,11 @@ class Toy {
 
     public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    public void decrementQuantity() {
+        if (quantity > 0) {
+            quantity--;
+        }
     }
 }
