4f2b512edbab38a3e622ae6fdb19b10ef23e62a9
Sergei <ip744692@yandex.ru>
2023-09-16 17:18:29 +0300

    add final ver

--- src/main/java/org/example/ToyStore.java
+++ src/main/java/org/example/ToyStore.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
@@ -11,13 +13,11 @@ public class ToyStore {
         prizeToys = new ArrayList<>();
     }
 
-    // Метод для добавления новой игрушки
     public void addToy(int id, String name, int quantity, int weight) {
         Toy toy = new Toy(id, name, quantity, weight);
         toys.add(toy);
     }
 
-    // Метод для изменения веса (частоты выпадения) игрушки
     public void updateWeight(int id, int newWeight) {
         for (Toy toy : toys) {
             if (toy.getId() == id) {
@@ -28,7 +28,6 @@ public class ToyStore {
         System.out.println("Игрушка с ID " + id + " не найдена.");
     }
 
-    // Метод для добавления призовой игрушки в список ожидания
     public void addPrizeToy(Toy toy) {
         if (toys.contains(toy) && toy.getQuantity() > 0) {
             prizeToys.add(toy);
@@ -37,11 +36,10 @@ public class ToyStore {
         }
     }
 
-    // Метод для получения призовой игрушки
     public Toy getPrizeToy() {
         if (!prizeToys.isEmpty()) {
-            Toy prize = prizeToys.remove(0); // Удаляем первую игрушку из списка призовых
-            prize.decrementQuantity(); // Уменьшаем количество игрушек
+            Toy prize = prizeToys.remove(0);
+            prize.decrementQuantity();
             return prize;
         } else {
             System.out.println("Список призовых игрушек пуст.");
@@ -49,7 +47,6 @@ public class ToyStore {
         }
     }
 
-    // Метод для записи призовой игрушки в текстовый файл
     public void writePrizeToTextFile(Toy prize, String fileName) {
         try (FileWriter fileWriter = new FileWriter(fileName, true)) {
             fileWriter.write("Победила игрушка: " + prize.getName() + "\n");
@@ -58,9 +55,7 @@ public class ToyStore {
         }
     }
 
-    // Метод для розыгрыша игрушек
     public Toy drawToy() {
-        // Создаем список с учетом весов (частот выпадения)
         List<Toy> weightedToys = new ArrayList<>();
         for (Toy toy : toys) {
             for (int i = 0; i < toy.getWeight(); i++) {
@@ -68,13 +63,11 @@ public class ToyStore {
             }
         }
 
-        // Выбираем случайную игрушку из списка с учетом весов
         Random random = new Random();
         int randomIndex = random.nextInt(weightedToys.size());
         return weightedToys.get(randomIndex);
     }
 
-    // Метод для определения начального количества всех игрушек в автомате
     public int getTotalQuantity() {
         int totalQuantity = 0;
         for (Toy toy : toys) {
@@ -87,7 +80,6 @@ public class ToyStore {
         ToyStore toyStore = new ToyStore();
         Scanner scanner = new Scanner(System.in);
 
-        // Ввод начального количества каждой игрушки с клавиатуры
         System.out.println("Введите начальное количество каждой игрушки в автомате:");
         System.out.print("Конструктор: ");
         int constructorQuantity = scanner.nextInt();
@@ -95,33 +87,29 @@ public class ToyStore {
         int robotQuantity = scanner.nextInt();
         System.out.print("Кукла: ");
         int dollQuantity = scanner.nextInt();
-
-        // Добавление новых игрушек
         toyStore.addToy(1, "Конструктор", constructorQuantity, 20);
         toyStore.addToy(2, "Робот", robotQuantity, 10);
         toyStore.addToy(3, "Кукла", dollQuantity, 70);
 
-        // Изменение веса (частоты) игрушек
         toyStore.updateWeight(1, 30);
 
-        // Вывод начального количества всех игрушек
         int initialTotalQuantity = toyStore.getTotalQuantity();
-        System.out.println("Начальное количество всех игрушек: " + initialTotalQuantity);
+        System.out.println("Начальное количество всех игрушек в автомате: " + initialTotalQuantity);
 
-        // Добавляем призовые игрушки в список ожидания
-        for (int i = 0; i < 5; i++) {
-            Toy prizeToy = toyStore.drawToy(); // Вызываем метод розыгрыша
+        for (int i = 0; i < initialTotalQuantity; i++) {
+            Toy prizeToy = toyStore.drawToy();
             if (prizeToy != null) {
                 toyStore.addPrizeToy(prizeToy);
             }
         }
 
-        // Выбор "играть" или "выход"
         while (true) {
             System.out.println("\nВыберите действие:");
             System.out.println("1. Играть");
-            System.out.println("2. Выход");
+            System.out.println("2. Добавить игрушку");
+            System.out.println("3. Выход");
             int choice = scanner.nextInt();
+
             if (choice == 1) {
                 Toy prize = toyStore.getPrizeToy();
                 if (prize != null) {
@@ -129,15 +117,25 @@ public class ToyStore {
                     System.out.println("Выдана призовая игрушка: " + prize.getName());
                 }
             } else if (choice == 2) {
+                System.out.println("Выберите игрушку для добавления:");
+                System.out.println("1. Конструктор");
+                System.out.println("2. Робот");
+                System.out.println("3. Кукла");
+                int toyChoice = scanner.nextInt();
+                if (toyChoice >= 1 && toyChoice <= 3) {
+                    toyStore.addToy(toyChoice, toyChoice == 1 ? "Конструктор" : toyChoice == 2 ? "Робот" : "Кукла", 0, toyChoice == 1 ? 20 : toyChoice == 2 ? 10 : 70);
+                } else {
+                    System.out.println("Некорректный выбор игрушки.");
+                }
+            } else if (choice == 3) {
                 break;
             } else {
-                System.out.println("Некорректный выбор. Пожалуйста, выберите 1 или 2.");
+                System.out.println("Некорректный выбор. Пожалуйста, выберите 1, 2 или 3.");
             }
         }
     }
 }
 
-// Класс для игрушек
 class Toy {
     private int id;
     private String name;
